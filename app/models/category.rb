b68818c45dde40e4c884be453c185313d51127fe
spodder123 <120617408+spodder123@users.noreply.github.com>
2023-07-24 11:33:52 -0500

    added pagination

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,3 +1,4 @@
 class Category < ApplicationRecord
   has_many :pokemons
+  paginates_per 10
 end
--- app/models/pokemon.rb
+++ app/models/pokemon.rb
@@ -1,4 +1,5 @@
 class Pokemon < ApplicationRecord
   belongs_to :category
   has_many :order_items
+  paginates_per 10
 end
