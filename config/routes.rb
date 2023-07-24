d29c1bfe23033631dbbc8726e65537e9697c2ef8
spodder123 <120617408+spodder123@users.noreply.github.com>
2023-07-24 09:04:23 -0500

    added pokemon controller

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,8 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  # root "articles#index"
+  # root "/"
+   get "/pokemons", to:"pokemons#index", as: "pokemons"
+   get "/pokemons/:id", to:"pokemons#show", as: "pokemon_show"
+
 end
