10d0bee0ab6747e9a746d63f4f81f7f14e7e2129
spodder123 <120617408+spodder123@users.noreply.github.com>
2023-07-24 09:45:32 -0500

    Added partials

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,8 @@
 class ApplicationController < ActionController::Base
+  before_action :find_categories
+
+  def find_categories
+    #@search_categories = Category.pluck:(:name)
+    @categories= Category.all()
+  end
 end
