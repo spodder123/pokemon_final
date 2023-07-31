cd40052ce3735bc9a6beb0793b2b38028a83ef3e
spodder123 <120617408+spodder123@users.noreply.github.com>
2023-07-31 16:06:45 -0500

    About and Contact Page

--- app/models/about.rb
+++ app/models/about.rb
@@ -1,4 +1,8 @@
 class About < ApplicationRecord
   validates :title, presence: true, length: { maximum: 255 }
   validates :body, presence: true
+
+  def self.ransackable_attributes(auth_object = nil)
+    ["body", "created_at", "id", "title", "updated_at"]
+  end
 end
--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,4 +1,9 @@
 class Contact < ApplicationRecord
   validates :title, presence: true, length: { maximum: 255 }
   validates :body, presence: true
+
+
+def self.ransackable_attributes(auth_object = nil)
+  ["body", "created_at", "id", "title", "updated_at"]
+end
 end
